cd0c4338dfe5de0289d9513cb51fa9fd2b39f910
lisa <150911313+ovcinnikovmatvej335@users.noreply.github.com>
2024-01-10 00:21:21 +0700

    Create void printBoard.swift

---
+++ void printBoard.swift
@@ -0,0 +1,8 @@
+void printBoard(const vector<vector<char>>& board) {
+    for (const auto& row : board) {
+        for (char cell : row) {
+            cout << cell << " ";
+        }
+        cout << endl;
+    }
+}
